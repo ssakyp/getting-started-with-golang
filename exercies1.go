36c7797fa2cf0db62624c836450bd0b6fa09e0fd
ssakyp <103821526+ssakyp@users.noreply.github.com>
2024-09-12 09:42:09 +0500

    Update exercies1.go

--- exercies1.go
+++ exercies1.go
@@ -1,8 +1,17 @@
 package main
 
+import "fmt"
+
 func main() {
   var firstName string = "Sultan"
   lastName := "Sakyp"
   fmt.Println(firstName," ", lastName)
+
+  currentYear := 2024
+  var birthYear int = 1996
+  age := currentYear - birthYear
+  nextYear := currentYear + 1
+  fmt.Println(age)
+  fmt.Println(nextYear)
 
 }
