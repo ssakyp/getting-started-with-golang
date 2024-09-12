f46945d41b8c15bd40a9546b85320f1fa845c3fe
ssakyp <103821526+ssakyp@users.noreply.github.com>
2024-09-12 09:17:38 +0500

    Update firstapp.go

--- firstapp.go
+++ firstapp.go
@@ -5,6 +5,8 @@ import "fmt"
 func main() {
   //var greetingText string
   //greetingText = "Hello World!"
-  var greetingText string = "Hello World!"
+  //var greetingText string = "Hello World!"
+  greetingText := "Hello World!"
   fmt.Println(greetingText)
+  //fmt.Println("Hello World!")
 }
