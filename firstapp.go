371026cae0c211d7cfe9a1dd34a9766c0ef85d24
ssakyp <103821526+ssakyp@users.noreply.github.com>
2024-09-13 09:57:29 +0500

    global variable greetingText

--- firstapp.go
+++ firstapp.go
@@ -2,12 +2,14 @@ package main
 
 import "fmt"
 
+var greetingText = "Hello World!"
+
 func main() {
   //fmt.Println("Hello World!")
   //var greetingText string
   //greetingText = "Hello World!"
   //var greetingText string = "Hello World!"
-  greetingText := "Hello World!"
+  
   fmt.Println(greetingText)
   
   luckyNumber := 17
