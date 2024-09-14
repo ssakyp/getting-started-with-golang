826196ab16a23d633d3e57e41a63eac779d4260c
ssakyp <103821526+ssakyp@users.noreply.github.com>
2024-09-14 15:49:39 +0000

    separating into files

--- bmi-calculator-app/bmi.go
+++ bmi-calculator-app/bmi.go
@@ -1,14 +1,11 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
 	"strconv"
 	"strings"
 )
 
-var reader = bufio.NewReader(os.Stdin)
 const mainTitle = "BMI Calculator"
 const separator = "------------------"
 const weightPrompt = "Please enter your weight in (kg):"
@@ -28,7 +25,7 @@ func main() {
 	//Save that user input in variables
 	weightInput = strings.Replace(weightInput, "\n", "", -1)
 	heightInput = strings.Replace(heightInput, "\n", "", -1)
-	weight, _:= strconv.ParseFloat(weightInput, 64)
+	weight, _ := strconv.ParseFloat(weightInput, 64)
 	height, _ := strconv.ParseFloat(heightInput, 64)
 	// fmt.Print(weightInput, heightInput)
 
---
+++ bmi-calculator-app/input.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"bufio"
+	"os"
+)
+
+
+var reader = bufio.NewReader(os.Stdin)
