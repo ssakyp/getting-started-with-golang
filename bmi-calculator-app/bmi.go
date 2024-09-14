41c6152b70233011ac695a18003e7fac8b8d849a
ssakyp <103821526+ssakyp@users.noreply.github.com>
2024-09-14 03:17:40 +0000

    output texts

--- bmi-calculator-app/bmi.go
+++ bmi-calculator-app/bmi.go
@@ -1,6 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+var reader = bufio.NewReader(os.Stdin)
 
 func main() {
 	//Output information
@@ -8,8 +14,10 @@ func main() {
 	fmt.Println("------------------")
 	//Prompt for user input (weight + height)
 	fmt.Println("Please inter your weight in (kg):")
+	weightInput, _ := reader.ReadString('\n')
 
 	fmt.Println("Please inter your height in (m):")
+	heightInput, _ := reader.ReadString('\n')
 	//Save that user input in variables
 	//Calculate the BMI  (weight / (height * heigh))
 	//Output the calculated BMI
