2ec4015064ac5731af542ae2577f626e782852b1
ssakyp <103821526+ssakyp@users.noreply.github.com>
2024-09-14 03:01:54 +0000

    update

--- bmi-calculator-app/bmi.go
+++ bmi-calculator-app/bmi.go
@@ -3,5 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World!")
+	//Output information
+	fmt.Println("BMI Calculator")
+	fmt.Println("------------------")
+	//Prompt for user input (weight + height)
+	fmt.Println("Please inter your weight in (kg):")
+
+	fmt.Println("Please inter your height in (m):")
+	//Save that user input in variables
+	//Calculate the BMI  (weight / (height * heigh))
+	//Output the calculated BMI
 }
