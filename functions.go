5252f5855e7950a9b507b4b3e497f6fa584b4166
ssakyp <103821526+ssakyp@users.noreply.github.com>
2024-09-15 06:16:35 +0000

    update function

--- functions.go
+++ functions.go
@@ -1,5 +1,18 @@
 package main
 
+import "fmt"
+
 func main() {
-	
+	a := 5
+	b := 6
+	sum := add(a, b)
+	printNumber(sum)
+}
+
+func add(num1, num2 int) int {
+	return num1 + num2
+}
+
+func printNumber(num int) {
+	fmt.Println(num)
 }
