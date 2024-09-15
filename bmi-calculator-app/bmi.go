6484c4e44430a95c5d4c86395a48d551f8ec10ec
ssakyp <103821526+ssakyp@users.noreply.github.com>
2024-09-15 10:52:55 +0000

    structurizing the code

--- bmi-calculator-app/bmi.go
+++ bmi-calculator-app/bmi.go
@@ -2,32 +2,17 @@ package main
 
 import (
 	"fmt"
-	"strconv"
-	"strings"
 
 	"github.com/ssakyp/bmi-calculator-app/info"
 )
 
 func main() {
 	//Output information
-	fmt.Println(info.MainTitle)
-	fmt.Println(info.Separator)
-	//Prompt for user input (weight + height)
-	fmt.Println(info.WeightPrompt)
-	weightInput, _ := reader.ReadString('\n')
-
-	fmt.Println(info.HeightPrompt)
-	heightInput, _ := reader.ReadString('\n')
-
-	//Save that user input in variables
-	weightInput = strings.Replace(weightInput, "\n", "", -1)
-	heightInput = strings.Replace(heightInput, "\n", "", -1)
-	weight, _ := strconv.ParseFloat(weightInput, 64)
-	height, _ := strconv.ParseFloat(heightInput, 64)
-	// fmt.Print(weightInput, heightInput)
-
+	info.PrintWelcome()
+	
+	weight, height := getUserMetrics()
 	//Calculate the BMI  (weight / (height * heigh))
 	bmi := weight / (height * height)
 	//Output the calculated BMI
-	fmt.Printf("Your Body Mass Index is %.2f", bmi)
+	fmt.Printf("Your Body Mass Index is %.2f\n", bmi)
 }
--- bmi-calculator-app/input.go
+++ bmi-calculator-app/input.go
@@ -2,8 +2,26 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"os"
+	"strconv"
+	"strings"
+
+	"github.com/ssakyp/bmi-calculator-app/info"
 )
 
-
 var reader = bufio.NewReader(os.Stdin)
+
+func getUserMetrics() (float64, float64) {
+	fmt.Println(info.WeightPrompt)
+	weightInput, _ := reader.ReadString('\n')
+
+	fmt.Println(info.HeightPrompt)
+	heightInput, _ := reader.ReadString('\n')
+
+	weightInput = strings.Replace(weightInput, "\n", "", -1)
+	heightInput = strings.Replace(heightInput, "\n", "", -1)
+	weight, _ := strconv.ParseFloat(weightInput, 64)
+	height, _ := strconv.ParseFloat(heightInput, 64)
+	return weight, height
+}
