739f7a3049ac4d45a47b70ef26c188787285acc9
ssakyp <103821526+ssakyp@users.noreply.github.com>
2024-09-16 01:39:57 +0000

    clean code update

--- bmi-calculator-app/input.go
+++ bmi-calculator-app/input.go
@@ -12,16 +12,16 @@ import (
 
 var reader = bufio.NewReader(os.Stdin)
 
-func getUserMetrics() (float64, float64) {
-	fmt.Println(info.WeightPrompt)
-	weightInput, _ := reader.ReadString('\n')
+func getUserMetrics() (weight float64, height float64) {
+	weight = getUserInput(info.WeightPrompt)
+	height = getUserInput(info.HeightPrompt)
+	return 
+}
 
-	fmt.Println(info.HeightPrompt)
-	heightInput, _ := reader.ReadString('\n')
-
-	weightInput = strings.Replace(weightInput, "\n", "", -1)
-	heightInput = strings.Replace(heightInput, "\n", "", -1)
-	weight, _ := strconv.ParseFloat(weightInput, 64)
-	height, _ := strconv.ParseFloat(heightInput, 64)
-	return weight, height
+func getUserInput(promptText string) (enteredValue float64) {
+	fmt.Println(promptText)
+	userInput, _ := reader.ReadString('\n')
+	userInput = strings.Replace(userInput, "\n", "", -1)
+	enteredValue, _ = strconv.ParseFloat(userInput, 64)
+	return
 }
