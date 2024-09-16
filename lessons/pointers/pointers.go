379df756a1c2fbbed78e02e32eaa07f4c7d9d624
ssakyp <103821526+ssakyp@users.noreply.github.com>
2024-09-16 09:00:17 +0500

    using pointers in functions

--- lessons/pointers/pointers.go
+++ lessons/pointers/pointers.go
@@ -13,12 +13,14 @@ func main() {
   fmt.Println(myAge) //0xc00001c070
   fmt.Println(*myAge) // 33 => dereferencing prints the value stored on that address
 
-  doubledAge := double(age)
+  doubledAge := double(myAge)
   fmt.Println(doubledAge) // 66
+  fmt.Println(age) //66
 }
 
-func double(number int) int {
-  result := number * 2
-  number = 100
+//go is creating a copy
+
+func double(number *int) int {
+  result := *number * 2
   return result
 }
