c8d8be3cf9a60ed2d5acbc0bed4416ba773fc422
ssakyp <103821526+ssakyp@users.noreply.github.com>
2024-09-16 08:50:56 +0500

    dereferencing

--- lessons/pointers/pointers.go
+++ lessons/pointers/pointers.go
@@ -5,7 +5,8 @@ func main() {
   age := 32
   fmt.Println(age) //32
 
-  var myAge *int
-  myAge = &age
+  //var myAge *int
+  myAge := &age
   fmt.Println(myAge) //0xc00001c070
+  fmt.Println(*myAge) // 32 => dereferencing prints the value stored on that address
 }
