4a8a7bce17ff0c0fee03c8203f0a17b8acef670a
ssakyp <103821526+ssakyp@users.noreply.github.com>
2024-09-16 08:55:44 +0500

    advantages of pointers

--- lessons/pointers/pointers.go
+++ lessons/pointers/pointers.go
@@ -6,8 +6,17 @@ func main() {
   fmt.Println(age) //32
 
   //var myAge *int
+  
   myAge := &age
   *myAge = 33 / changing the value
+  
   fmt.Println(myAge) //0xc00001c070
   fmt.Println(*myAge) // 33 => dereferencing prints the value stored on that address
+
+  doubledAge := double(age)
+  fmt.Println(doubledAge) // 66
+}
+
+func double(number int) int {
+  return number *2
 }
