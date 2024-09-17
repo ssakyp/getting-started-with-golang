0b91ddeed2c0fc2bd80d56c8c8c7a66719234055
ssakyp <103821526+ssakyp@users.noreply.github.com>
2024-09-17 10:05:06 +0500

    Update practice.go

--- exercises/practice/practice.go
+++ exercises/practice/practice.go
@@ -3,34 +3,35 @@ package main
 import "fmt"
 
 type Book struct {
-  id int
-  title string
-  shortDescription string
-  price float64
+	id               int
+	title            string
+	shortDescription string
+	price            float64
 }
 
 func NewBook(id int, title string, shortDescription string, price float64) *Book {
-  newBook := Book {
-    id: id,
-    title: title,
-    shortDescription: shortDescription,
-    pirce: price,
-  }
-  return &newBook
+	newBook := Book{
+		id:               id,
+		title:            title,
+		shortDescription: shortDescription,
+		price:            price,
+	}
+	return &newBook
 }
 
 func (book *Book) outputDetails() {
-  fmt.Printf("The %v is %v with id of %v costs %v", book.title, book.shortDescription, book.id, book.price)
+	fmt.Printf("The %v is %v with id of %v costs %v", book.title, book.shortDescription, book.id, book.price)
 }
 
 func main() {
-  var newBook Book {
-    24,
-    "A book"
-    "Very interesting book"
-    25.4
-  }
-  fmt.Println(newBook)
-  anotherBook := NewBook(29, "The second Book", "Not that much interesting", 9.01)
-  anotherBook.outputDetails()
+	var newBook Book
+	newBook = Book{
+		24,
+		"A book",
+		"Very interesting book",
+		25.4,
+	}
+	fmt.Println(newBook)
+	anotherBook := NewBook(29, "The second Book", "Not that much interesting", 9.01)
+	anotherBook.outputDetails()
 }
