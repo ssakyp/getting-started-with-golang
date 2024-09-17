5cac2d4b10df7f2081a711286de28750a1a7b8a4
ssakyp <103821526+ssakyp@users.noreply.github.com>
2024-09-17 10:01:05 +0500

    Update practice.go

--- exercises/practice/practice.go
+++ exercises/practice/practice.go
@@ -1,5 +1,34 @@
 package main
 
+type Book struct {
+  id int
+  title string
+  shortDescription string
+  price float64
+}
+
+func NewBook(id int, title string, shortDescription string, price float64) *Book {
+  newBook := Book {
+    id: id
+    title: title
+    shortDescription: shortDescription
+    pirce: price
+  }
+  return &newBook
+}
+
+func (book *Book) outputDetails() {
+  fmt.Printf("The %v is %v with id of %v costs %v", book.title, book.shortDescription, book.id, book.price)
+}
+
 func main() {
-  
+  var newBook Book {
+    24,
+    "A book"
+    "Very interesting book"
+    25.4
+  }
+  fmt.Println(newBook)
+  anotherBook := NewBook(29, "The second Book", "Not that much interesting", 9.01)
+  anotherBook.outputDetails()
 }
