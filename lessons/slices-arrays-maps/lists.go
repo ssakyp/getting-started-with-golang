94d52ad3a61a6075077f34b7c8276ab42e3f5e78
ssakyp <103821526+ssakyp@users.noreply.github.com>
2024-09-18 10:20:53 +0500

    Update lists.go

--- lessons/slices-arrays-maps/lists.go
+++ lessons/slices-arrays-maps/lists.go
@@ -36,7 +36,9 @@ func main() {
 
   featuredPrices := prices[1:3] //slice => start at index 1 and excluding index 3
   fmt.Println(featuredPrices) //[93.11 45.11]
-  
+
+  secondFeaturedPrices := prices[:3] // slice start from the beginning up to 3 but excluding it
+  fmt.Println(secondFeaturedPrices) // [10.99 93.11 45.11]
 }
 
 
