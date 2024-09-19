3f867913e86488471c5200da5bb0181d09a754e6
ssakyp <103821526+ssakyp@users.noreply.github.com>
2024-09-19 09:21:04 +0500

    Update dynamic-arrays.go

--- lessons/slices-arrays-maps/dynamic-arrays.go
+++ lessons/slices-arrays-maps/dynamic-arrays.go
@@ -3,16 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	hobbies := [3]string{"tennis", "filmmaking", "coding"}
-	fmt.Println(hobbies)
-	fmt.Println(hobbies[0])
-	fmt.Println(hobbies[1:])
-	var firstHobbies []string
-	firstHobbies = hobbies[0:2]
-	secondHobbies := []string{hobbies[0], hobbies[1]}
-	fmt.Println(firstHobbies)
-	fmt.Println(secondHobbies)
-	firstHobbies = firstHobbies[1:]
-	fmt.Println(firstHobbies)
+	prices := []float64{10.99, 8.99}
+	fmt.Println(prices[0:1]) // 10.99
+	prices[1] = 9.99
+
+	prices = append(prices, 5.99, 12.99, 29.99, 100.10)
+	prices = prices[1:]
+	fmt.Println(prices) // [9.99 5.99 12.99 29.99 100.10]
+
+	discountPrices := []float64{101.99, 80.99, 20.59}
+	prices = append(prices, discountPrices...)
+	fmt.Println(prices) // [9.99 5.99 12.99 29.99 100.10 101.99 80.99 20.59]
+	
 
 }
