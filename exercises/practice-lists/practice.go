b8a089396bb37c75234735bc7f78e067ccdc7e1e
ssakyp <103821526+ssakyp@users.noreply.github.com>
2024-09-19 08:46:37 +0500

    Update practice.go

--- exercises/practice-lists/practice.go
+++ exercises/practice-lists/practice.go
@@ -3,17 +3,16 @@ package main
 import "fmt"
 
 func main() {
-  hobbies := [3]string{"tennis", "filmmaking", "coding"}
-  fmt.Println(hobbies)
-  fmt.Println(hobbies[0])
-  fmt.Println([1:])
-  var firstHobbies []string {}
-  firstHobbies = hobbies[0:2]
-  secondHobbies := []string{hobbies[0], hobbies[1]}
-  fmt.Println(firstHobbies)
-  fmt.Println(secondHobbies)
-  firstHobbies = firsHobbies[1:]
-  fmt.Println(firstHobbies)
-  
-  
+	hobbies := [3]string{"tennis", "filmmaking", "coding"}
+	fmt.Println(hobbies)
+	fmt.Println(hobbies[0])
+	fmt.Println(hobbies[1:])
+	var firstHobbies []string
+	firstHobbies = hobbies[0:2]
+	secondHobbies := []string{hobbies[0], hobbies[1]}
+	fmt.Println(firstHobbies)
+	fmt.Println(secondHobbies)
+	firstHobbies = firstHobbies[1:3]
+	fmt.Println(firstHobbies)
+
 }
