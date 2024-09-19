71107a631fad8ef869d08efc33aa5090270399ef
ssakyp <103821526+ssakyp@users.noreply.github.com>
2024-09-19 08:32:05 +0500

    Update dynamic-arrays.go

--- lessons/slices-arrays-maps/dynamic-arrays.go
+++ lessons/slices-arrays-maps/dynamic-arrays.go
@@ -6,5 +6,11 @@ func main() {
   prices := []float64{10.99, 8.99}
   fmt.Println(prices[1]) //8.99
   fmt.Println(prices[0:1]) // 10.99
+  prices[1] = 1.11
+  //prices[2] => out of range => panics
+  updatedPrices := append(prices, 5.99)
+  prices = append(prices, 4.87)
+  fmt.Println(updatedPrices) // [10.99 1.11 5.99]
+  fmt.Println(prices) // [10.99 1.11 4.87]
   
 }
