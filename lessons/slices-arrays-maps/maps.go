c0498631b6a40ccad1808dda90bbd3669b4f96dd
ssakyp <103821526+ssakyp@users.noreply.github.com>
2024-09-19 09:26:59 +0500

    Update maps.go

--- lessons/slices-arrays-maps/maps.go
+++ lessons/slices-arrays-maps/maps.go
@@ -1,5 +1,14 @@
 package main
 
+import "fmt"
+
 func main() {
+  // websites := []string{"https://google.com", "https://aws.com"} this would be problem
+  websites := map[string]string{
+    "Google": "https://google.com",
+    "Amazon Web Services": "https://aws.com",
+  }
+  fmt.Println(websites)
+  
   
 }
