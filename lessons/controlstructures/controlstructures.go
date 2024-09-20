9da5dba49c8e827f981b1d26771860b5f7eb8d52
ssakyp <103821526+ssakyp@users.noreply.github.com>
2024-09-20 06:22:18 +0500

    Update controlstructures.go

--- lessons/controlstructures/controlstructures.go
+++ lessons/controlstructures/controlstructures.go
@@ -3,6 +3,7 @@ package main
 import (
   "os"
   "bufio"
+  "fmt"
 )
 
 func main() {
@@ -12,10 +13,12 @@ func main() {
   userAgeInput = strings.Replace(userAgeInput, "\n", "", -1)
   userAge, _ := strconv.ParseInt(userAgeInput, 0, 64)
 
-  isOldEnough := userAge >= 18
+  // isOldEnough := userAge >= 18
   
-  if isOldEnough {
+  if userAge >= 18 {
     fmt.Println("Welcome to the club!")
+  } else {
+    fmt.Println("You're not old enough!")
   }
 
   fmt.Println("Goodbye!")
