51612073c863da852f41388d106db6332cbb06ce
ssakyp <103821526+ssakyp@users.noreply.github.com>
2024-09-20 06:40:22 +0500

    Update controlstructures.go

--- lessons/controlstructures/controlstructures.go
+++ lessons/controlstructures/controlstructures.go
@@ -6,20 +6,13 @@ import (
   "fmt"
 )
 
-func main() {
-  reader := bufio.NewReader(os.Stdin)
-  fmt.Print("Please enter your age: ")
-  userAgeInput := reader.ReadString('\n')
-  userAgeInput = strings.Replace(userAgeInput, "\n", "", -1)
-  userAge, err := strconv.ParseInt(userAgeInput, 0, 64)
-
+func main() {  
+  // isOldEnough := userAge >= 18
+  userAge, err := getUserAge()
   if err != nil {
     fmt.Println(err)
     return // to stop the execution of the function
   }
-  
-  // isOldEnough := userAge >= 18
-  
   if (userAge >= 30 && userAge < 50) || userAge >= 50 {
     fmt.Println("You are eligible for our senior jobs.")
   } else if userAge >= 50 {
@@ -31,4 +24,15 @@ func main() {
   }
 
   fmt.Println("Goodbye!")
+}
+
+func getUserAge() (int, error){
+   reader := bufio.NewReader(os.Stdin)
+  fmt.Print("Please enter your age: ")
+  userAgeInput := reader.ReadString('\n')
+  userAgeInput = strings.Replace(userAgeInput, "\n", "", -1)
+  userAge, err := strconv.ParseInt(userAgeInput, 0, 64)
+  
+  // error := erros.New("Invalid input!")
+  return int(userAge), err
 }
