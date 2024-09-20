8265db55fc999c4afb13c5e9728794b6b9e3dab6
ssakyp <103821526+ssakyp@users.noreply.github.com>
2024-09-20 06:58:20 +0500

    Update loops.go

--- lessons/controlstructures/loops.go
+++ lessons/controlstructures/loops.go
@@ -9,9 +9,31 @@ import (
 )
 
 var reader bufio.NewReader(os.Stdin)
+
 func main() {
-  
+  selectedChoice, err := getUserChoice()
+
+  if err != nil {
+    fmt.Println("Invalid choice, exiting")
+    return
+  }
+
+  if selectedChoice == "1" {
+    calculateSumUpToNumber()
+  } else if selectedChoice == "2" {
+    calculateFactorial()
+  } else if selectedChoice == "3" {
+    calculateSumManually()
+  } else {
+    calculateListSum()
+  }
 }
+
+func calculateSumUpToNumber() {}
+func calculateFactorial() {}
+func calculateSumManually() {}
+func calculateListSum() {}
+
 
 func getUserChoice() (string, error) {
   fmt.Println("Please make your choice")
@@ -31,6 +53,6 @@ func getUserChoice() (string, error) {
   if userInput == "1" || userInput == "2" || userInput == "3" || userInput == "4" {
     return userInput, nil
   } else {
-    return "", errors.New("Invalid input!")
+    return "", errors.New("INVALID INPUT")
   }
 }
